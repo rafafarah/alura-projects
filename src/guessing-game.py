bf99c2b95262ef32f1ec7bf46edb9e8ce921a6be
Rafael Farah <rafaelfarahster@gmail.com>
2021-03-28 01:18:09 -0300

    Adding loop for multiple guesses

--- src/guessing-game.py
+++ src/guessing-game.py
@@ -4,15 +4,23 @@ print("****************************")
 
 secret_number = 42
 
-guess = int(input("Insert number: "))
+total_of_guess = 3
+guess_round = 1
 
-guessed_right   = guess == secret_number
-is_higher       = guess > secret_number
+while (total_of_guess >= guess_round):
+    print("Round", guess_round, "of", total_of_guess)
+    guess = int(input("Insert number: "))
 
-if (guessed_right):
-    print("Congrats")
-else:
-    if (is_higher):
-        print("Not this time... Your guess is too high.")
+    guessed_right   = guess == secret_number
+    is_higher       = guess > secret_number
+
+    if (guessed_right):
+        print("Congrats")
+        break
     else:
-        print("Not this time... Your guess is too low.")
+        if (is_higher):
+            print("Not this time... Your guess is too high.")
+        else:
+            print("Not this time... Your guess is too low.")
+
+    guess_round += 1
