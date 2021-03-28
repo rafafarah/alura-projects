afca13be9e67051b5436b3e695f1201f8612a44b
Rafael Farah <rafaelfarahster@gmail.com>
2021-03-28 01:39:44 -0300

    Adding limits for the guesses and checking it before testing it

--- src/guessing-game.py
+++ src/guessing-game.py
@@ -8,7 +8,11 @@ total_of_guess = 3
 
 for guess_round in range(1, total_of_guess+1):
     print("Round {} of {}".format(guess_round, total_of_guess))
-    guess = int(input("Insert number: "))
+    guess = int(input("Insert number between 1 and 100: "))
+
+    if (guess < 1 or guess > 100):
+        print("Guesses must be between 1 and 100")
+        continue
 
     guessed_right   = guess == secret_number
     is_higher       = guess > secret_number
