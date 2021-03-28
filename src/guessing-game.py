2299704d219af38d7ef088ad1d78b72e73f70d01
Rafael Farah <rafaelfarahster@gmail.com>
2021-03-28 00:35:58 -0300

    Adding initial guessing logic

--- src/guessing-game.py
+++ src/guessing-game.py
@@ -1,1 +1,12 @@
+print("****************************")
 print("Welcome to the Guessing Game")
+print("****************************")
+
+secret_number = 42
+
+guess = int(input("Insert number: "))
+
+if (secret_number == guess):
+    print("Congrats")
+else:
+    print("Not this time...")
