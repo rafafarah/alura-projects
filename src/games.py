79bb67193d2dbdc98ff9e643943732c8069753dd
Rafael Farah <rafaelfarahster@gmail.com>
2021-03-28 23:31:34 -0300

    Adding function select_game

--- src/games.py
+++ src/games.py
@@ -1,15 +1,19 @@
 import guessing_game
 
-print("****************************")
-print("*Welcome to Nostalgic Game!*")
-print("****************************")
+def select_game():
+    print("****************************")
+    print("*Welcome to Nostalgic Game!*")
+    print("****************************")
 
-print("(1) Guessing game")
+    print("(1) Guessing game")
 
-game = int(input("Select a game to play:"))
+    game = int(input("Select a game to play: "))
 
-if (game == 1):
-    print("Guessing game selected")
-    guessing_game.play()
-else:
-    print("Invalid game")
+    if (game == 1):
+        print("Guessing game selected")
+        guessing_game.play()
+    else:
+        print("Invalid game")
+
+if (__name__ == "__main__"):
+    select_game()
