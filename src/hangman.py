9190404ddc517e6e336482ef9b577f43df6ceee1
Rafael Farah <rafaelfarahster@gmail.com>
2021-03-29 23:48:07 -0300

    Adding Hangman game initial file

---
+++ src/hangman.py
@@ -0,0 +1,15 @@
+def play():
+    print("***************************")
+    print("Welcome to the Hangman Game")
+    print("***************************")
+
+    secret_word = "banana"
+    hanged = False
+    won = False
+
+    while (not hanged and not won):
+        print("playing")
+
+
+if (__name__ == "__main__"):
+    play()
