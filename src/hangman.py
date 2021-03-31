62d24198e321bb5da09ddb17cb6da2572517b5fe
Rafael Farah <rafaelfarahster@gmail.com>
2021-03-30 23:33:30 -0300

    Adding input and guess checks

--- src/hangman.py
+++ src/hangman.py
@@ -8,19 +8,33 @@ def play():
 
     hanged = False
     won = False
+    misses = 0
 
     print(guessed_right)
     while (not hanged and not won):
         guess = input("\nYour guess: ")
-        guess = guess.strip()
+        guess = guess.strip().upper()
 
-        index = 0
-        for letter in secret_word:
-            if (guess.upper() == letter.upper()):
-                guessed_right[index] = letter
-            index += 1
+        if (guess in secret_word):
+            index = 0
+            for letter in secret_word:
+                if (guess == letter):
+                    guessed_right[index] = letter
+                index += 1
+        else:
+            misses += 1
+            print("Opps, you missed! {} guesses remaining.".format(6-misses))
+
+        hanged = (misses == 6)
+
+        won = ("_" not in guessed_right)
 
         print(guessed_right)
+
+    if (hanged):
+        print("Game Over!")
+    else:
+        print("You won!")
 
 
 if (__name__ == "__main__"):
