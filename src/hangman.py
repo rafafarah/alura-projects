472aac0068b9a3d59b3bc752cedab5167ce9e34c
Rafael Farah <rafaelfarahster@gmail.com>
2021-03-30 22:56:05 -0300

    Printing letter guessed right

--- src/hangman.py
+++ src/hangman.py
@@ -3,18 +3,24 @@ def play():
     print("Welcome to the Hangman Game")
     print("***************************")
 
-    secret_word = "python"
+    secret_word = "banana"
+    guessed_right = ["_", "_", "_", "_", "_", "_"]
+
     hanged = False
     won = False
 
+    print(guessed_right)
     while (not hanged and not won):
         guess = input("\nYour guess: ")
+        guess = guess.strip()
 
+        index = 0
         for letter in secret_word:
-            if (guess == letter):
-                print(guess, end="")
-            else:
-                print("_", end="")
+            if (guess.upper() == letter.upper()):
+                guessed_right[index] = letter
+            index += 1
+
+        print(guessed_right)
 
 
 if (__name__ == "__main__"):
