982c5bb8736793ac8365b9d3a9cd94a832b55da3
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-02 02:24:01 -0300

    Refactoring function play

--- src/hangman.py
+++ src/hangman.py
@@ -1,10 +1,40 @@
 import random
 
 def play():
+    print_welcome_message()
+    secret_word = load_secret_word()
+
+    guessed_right = initialize_guessed_right(secret_word)
+    print(guessed_right)
+
+    hanged = False
+    won = False
+    misses = 0
+
+    while (not hanged and not won):
+        guess = read_guess()
+
+        if (guess in secret_word):
+            fill_guessed_right(guess, secret_word, guessed_right)
+        else:
+            misses += 1
+            print("Opps, you missed! {} guesses remaining.".format(6-misses))
+
+        hanged = (misses == 6)
+        won = ("_" not in guessed_right)
+        print(guessed_right)
+
+    if (hanged):
+        print_game_over()
+    else:
+        print_win_message()
+
+def print_welcome_message():
     print("***************************")
     print("Welcome to the Hangman Game")
     print("***************************")
 
+def load_secret_word():
     fileword = open("words.txt", "r")
     words = []
 
@@ -12,42 +42,29 @@ def play():
         words.append(line.strip())
 
     fileword.close()
-
     fileindex = random.randrange(0, len(words))
-
     secret_word = words[fileindex].upper()
-    guessed_right = ["_" for letter in secret_word]
+    return secret_word
 
-    hanged = False
-    won = False
-    misses = 0
+def initialize_guessed_right(secret):
+    return ["_" for letter in secret]
 
-    print(guessed_right)
-    while (not hanged and not won):
-        guess = input("\nYour guess: ")
-        guess = guess.strip().upper()
+def read_guess():
+    guess = input("\nYour guess: ")
+    return guess.strip().upper()
 
-        if (guess in secret_word):
-            index = 0
-            for letter in secret_word:
-                if (guess == letter):
-                    guessed_right[index] = letter
-                index += 1
-        else:
-            misses += 1
-            print("Opps, you missed! {} guesses remaining.".format(6-misses))
+def fill_guessed_right(guess, secret_word, guessed_right):
+    index = 0
+    for letter in secret_word:
+        if (guess == letter):
+            guessed_right[index] = letter
+        index += 1
 
-        hanged = (misses == 6)
+def print_game_over():
+    print("Game Over!")
 
-        won = ("_" not in guessed_right)
-
-        print(guessed_right)
-
-    if (hanged):
-        print("Game Over!")
-    else:
-        print("You won!")
-
+def print_win_message():
+    print("You won!")
 
 if (__name__ == "__main__"):
     play()
