525d17827c2a5476df0fdf39ebf6c5895efd40f3
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-02 23:03:38 -0300

    Adding Hangman to game menu

--- src/games.py
+++ src/games.py
@@ -1,17 +1,21 @@
 import guessing_game
+import hangman
 
 def select_game():
     print("****************************")
     print("*Welcome to Nostalgic Game!*")
     print("****************************")
 
-    print("(1) Guessing game")
+    print("(1) Guessing game, (2) Hangman")
 
     game = int(input("Select a game to play: "))
 
-    if (game == 1):
+    if (1 == game):
         print("Guessing game selected")
         guessing_game.play()
+    elif (2 == game):
+        print("Hangman game selected")
+        hangman.play()
     else:
         print("Invalid game")
 
