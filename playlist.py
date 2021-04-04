ef0ae09969ebf1623d4854982c6d0e62a5e08c0d
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-04 16:13:18 -0300

    Adding __getitem__ and __len__ to playlist

--- playlist.py
+++ playlist.py
@@ -3,11 +3,9 @@ class Playlist:
     def __init__(self, name, shows):
         self._name = name
         self._shows = shows
-    
-    @property
-    def shows(self):
-        return self._shows
-    
-    @property
-    def len(self):
+
+    def __getitem__(self, item):
+        return self._shows[item]
+
+    def __len__(self):
         return len(self._shows)
