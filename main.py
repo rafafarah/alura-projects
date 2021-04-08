a0f9b26072bcd93626f99c1f4bb5a81a70cc5245
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-08 00:45:31 -0300

    Adding functions to convert contact to pickle and json

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import contact_util
+
 def writing_using_try_finally():
     # important to use try/finally to make sure file will be closed 
     # even if an exception is issued
@@ -38,7 +40,7 @@ def writing_using_try_finally():
 
 def writing_using_with_as():
     try:
-        with open("data/contacts_iso8859-1.csv", encoding='latin_1') as contact_file:
+        with open('data/contacts_iso8859-1.csv', encoding='latin_1') as contact_file:
             for line in contact_file:
                 print(line, end='')
     except FileNotFoundError:
@@ -47,4 +49,53 @@ def writing_using_with_as():
         print('Writing permission denied')
 
 
-writing_using_with_as()
+def reading_file_using_csv_to_contacts():
+    try:
+        contacts = contact_util.csv_to_contacts('data/contacts_iso8859-1.csv')
+
+        for contact in contacts:
+            print(f'{contact.id} - {contact.name} - {contact.email}')
+
+    except FileNotFoundError:
+        print('File not found')
+    except PermissionError:
+        print('Writing permission denied')
+
+
+def convert_contacts_to_pickle():
+    try:
+        # reads csv and convert it to contacts
+        contacts = contact_util.csv_to_contacts('data/contacts_iso8859-1.csv')
+        # save contacts to pickle file
+        contact_util.contacts_to_pickle(contacts, 'data/contacts.pickle')
+        # reads from pickle and convert it back to contacts
+        contacts = contact_util.pickle_to_contacts('data/contacts.pickle')
+
+        for contact in contacts:
+            print(f'{contact.id} - {contact.name} - {contact.email}')
+
+    except FileNotFoundError:
+        print('File not found')
+    except PermissionError:
+        print('Writing permission denied')
+
+
+def convert_contacts_to_json():
+    try:
+        # reads csv and convert it to contacts
+        contacts = contact_util.csv_to_contacts('data/contacts_iso8859-1.csv')
+        # save contacts to json
+        contact_util.contacts_to_json(contacts, 'data/contacts.json')
+        # reads from json and convert it back to contacts
+        contacts = contact_util.json_to_contact('data/contacts.json')
+
+        for contact in contacts:
+            print(f'{contact.id} - {contact.name} - {contact.email}')
+
+    except FileNotFoundError:
+        print('File not found')
+    except PermissionError:
+        print('Writing permission denied')
+
+
+convert_contacts_to_json()
