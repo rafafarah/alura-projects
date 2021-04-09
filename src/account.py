9993f74c6575b379c19837dda0d6923a394fc04c
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-09 19:24:21 -0300

    Adding getters/setter and removing account test code

--- src/account.py
+++ src/account.py
@@ -10,13 +10,48 @@ class Account:
         Account.total_accounts += 1
         Account.operation_tax = 30 / Account.total_accounts
 
+    @property
+    def ag(self):
+        return self.__ag
+
+    def __set_ag(self, ag):
+        if (not isinstance(ag, int)):
+            raise TypeError('Attribute must be an integer', ag)
+        if (ag <=0):
+            raise ValueError('Attribute must be greater than zero')
+
+        self.__ag = ag
+
+    @property
+    def number(self):
+        return self.__number
+
+    def __set_number(self, number):
+        if (not isinstance(number, int)):
+            raise TypeError('Attribute must be an integer')
+        if (number <=0):
+            raise ValueError('Attribute must be greater than zero')
+
+        self.__number = number
+
+    @property
+    def balance(self):
+        return self.__balance
+    @balance.setter
+    def balance(self, balance):
+        if (not isinstance(balance, int)):
+            raise TypeError('Attribute must be an integer')
+        if (number <0):
+            raise ValueError('Attribute must be positive')
+
+        self.__balance = balance
+
     def transfer(self, value, dest):
         dest.deposit(value)
 
     def withdraw(self, value):
-        self.balance -= value
+        self.__balance -= value
 
     def deposit(self, value):
-        self.balance += value
+        self.__balance += value
 
-cc = Account(None, '00', '101')
