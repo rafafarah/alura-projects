6fa5f6823f1ab8c5365529a8b717fbdfa28bc7af
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-12 00:28:47 -0300

    Ading TEST_CASE("Recuperar menor lance de leilão em ordem crescente")

--- Leilao/tests/teste_avaliador.cpp
+++ Leilao/tests/teste_avaliador.cpp
@@ -34,3 +34,19 @@ TEST_CASE("Recuperar maior lance de leilão em ordem decrescente") {
     // Assert: verificando a saída esperada
     REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
 }
+
+TEST_CASE("Recuperar menor lance de leilão em ordem crescente") {
+    // Arrange: preparando ambiente
+    Lance primeiroLance(Usuario("Jorge"), 1000);
+    Lance segundoLance(Usuario("Jorgita"), 2000);
+    Leilao leilao("Fiat 147 0Km");
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+    Avaliador leiloeiro;
+
+    // Act: executando o código a ser testado
+    leiloeiro.avalia(leilao);
+
+    // Assert: verificando a saída esperada
+    REQUIRE(1000 == leiloeiro.recuperaMenorValor());
+}
