bd3235469c3ed146d25952f702c14ba8c235a48b
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-12 00:24:47 -0300

    Replacing main() to TEST_CASE("Recuperar maior lance de leilão em ordem crescente")

--- Leilao/tests/teste_avaliador.cpp
+++ Leilao/tests/teste_avaliador.cpp
@@ -1,10 +1,9 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include "Avaliador.hpp"
-#include "Usuario.hpp"
-#include "Lance.hpp"
-#include "Leilao.hpp"
 #include <iostream>
 
-int main () {
+TEST_CASE("Recuperar maior lance de leilão em ordem crescente") {
     // Arrange: preparando ambiente
     Lance primeiroLance(Usuario("Jorge"), 1000);
     Lance segundoLance(Usuario("Jorgita"), 2000);
@@ -17,12 +16,5 @@ int main () {
     leiloeiro.avalia(leilao);
 
     // Assert: verificando a saída esperada
-    float valorEsperado = 2000;
-    if (valorEsperado == leiloeiro.recuperaMaiorValor()) {
-        std::cout << "TESTE OK" << std::endl;
-    } else {
-        std::cout << "TESTE FALHOU" << std::endl;
-    }
-
-    return 0;
+    REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
 }
