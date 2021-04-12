15aa7e00bf31dc510097b7691e7b1eef370b4eae
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-12 00:59:54 -0300

    Adding TEST_CASE("Recuperar 3 maiores lances")

--- Leilao/tests/teste_avaliador.cpp
+++ Leilao/tests/teste_avaliador.cpp
@@ -66,3 +66,28 @@ TEST_CASE("Recuperar menor lance de leilão em ordem decrescente") {
     // Assert: verificando a saída esperada
     REQUIRE(1000 == leiloeiro.recuperaMenorValor());
 }
+
+TEST_CASE("Recuperar 3 maiores lances") {
+    // Arrange: preparando ambiente
+    Lance primeiroLance(Usuario("Jorge"), 1000);
+    Lance segundoLance(Usuario("Jorgita"), 2000);
+    Lance terceiroLance(Usuario("Jefferson"), 1500);
+    Lance quartoLance(Usuario("Jenifer"), 2500);
+
+    Leilao leilao("Fiat 147 0Km");
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+    leilao.recebeLance(terceiroLance);
+    leilao.recebeLance(quartoLance);
+    Avaliador leiloeiro;
+
+    // Act: executando o código a ser testado
+    leiloeiro.avalia(leilao);
+
+    // Assert: verificando a saída esperada
+    auto maiores3Lances = leiloeiro.recupera3MaioresLances();
+    REQUIRE(3 == maiores3Lances.size());
+    REQUIRE(2500 == maiores3Lances[0].recuperaValor());
+    REQUIRE(2000 == maiores3Lances[1].recuperaValor());
+    REQUIRE(1500 == maiores3Lances[2].recuperaValor());
+}
