5edf1a6e814bd95ecb0e0542a5f7895b41986e17
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-12 22:45:18 -0300

    Adding TEST_CASE("Leilão não deve receber lances consecutivos do mesmo usuário")

--- Leilao/tests/teste_avaliador.cpp
+++ Leilao/tests/teste_avaliador.cpp
@@ -73,3 +73,20 @@ TEST_CASE("Avaliador") {
         REQUIRE(1500 == maiores3Lances[2].recuperaValor());
     }
 }
+
+TEST_CASE("Leilão não deve receber lances consecutivos do mesmo usuário") {
+    // Arrange
+    Leilao leilao("Fiat 147 0Km");
+    Usuario usuario("Jorge");
+
+    Lance primeiroLance(usuario, 1000);
+    Lance segundoLance(usuario, 1500);
+
+    // Act
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+
+    // Assert
+    REQUIRE(1 == leilao.recuperaLances().size());
+    REQUIRE(1000 == leilao.recuperaLances()[0].recuperaValor());
+}
