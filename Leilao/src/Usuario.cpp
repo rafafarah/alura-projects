31574c5a096de9d840dd57c5a68325583e531712
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-12 22:43:49 -0300

    Adding recuperaPrimeiroNome() to Usuario

--- Leilao/src/Usuario.cpp
+++ Leilao/src/Usuario.cpp
@@ -8,3 +8,8 @@ std::string Usuario::recuperaNome() const
 {
     return nome;
 }
+
+std::string Usuario::recuperaPrimeiroNome() const
+{
+    return nome.find(' ') != -1 ? nome.substr(0, nome.find(' ')) : nome;
+}
