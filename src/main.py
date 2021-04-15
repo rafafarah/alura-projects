cd3216e141aeaacaa281706d85eb6c2795aaeeef
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-15 20:41:45 -0300

    Commenting tests to split/slice string

--- src/main.py
+++ src/main.py
@@ -2,17 +2,16 @@
 # # slicing aboutMe to get only the age
 # substrig = aboutMe[25:28]
 # print(substrig)
-
-
 # # finding index for arguments
 # url = "https:://www.bytebank.com.br/exchange?src=brl&dest=usd&value=1500"
 # src_idx = url.find("src=")
 # dest_idx = url.find("dest=")
 # src = url[src_idx+4:dest_idx-1]
 # print(src)
+# # spliting string using a separator
+# url = "https:://www.bytebank.com.br/exchange?src=brl&dest=usd&value=1500"
+# listArg = url.split("=")
+# print(listArg)
 
 
-# spliting string using a separator
-url = "https:://www.bytebank.com.br/exchange?src=brl&dest=usd&value=1500"
-listArg = url.split("=")
-print(listArg)
+
