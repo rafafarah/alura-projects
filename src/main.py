46e6a928b2cb49797fda14f6a43108f320a2f643
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-19 23:25:04 -0300

    Adding test_polymorphism test_array test_numpy

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from account import Account
+from account import Account, CheckingAccount, SavingAccount
 
 def test_account_list():
     ac1 = Account(15)
@@ -11,6 +11,32 @@ def test_account_list():
     accounts = [ac1, ac2]
     for ac in accounts:
         print(ac)
+
+def test_polymorphism():
+    ac15 = CheckingAccount(15)
+    ac15.deposit(1000)
+
+    ac16 = SavingAccount(16)
+    ac16.deposit(1000)
+
+    acs = [ac15, ac16]
+
+    for ac in acs:
+        ac.after_mount()
+        print(ac)
+
+# avoid array unless performance is needed
+def test_array():
+    import array as arr
+    ar = arr.array('d', [1, 1.5])
+    print(ar)
+
+def test_numpy():
+    import numpy as np
+    np_array = np.array([1, 1.3])
+    print(np_array)
+    np_array += 3
+    print(np_array)
 
 
 if __name__ == "__main__":
