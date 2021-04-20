8951db4be59b0088e7055669912d87a05568b5ff
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-19 23:24:27 -0300

    Spliting main.py into test funcitons

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 from account import Account
 
-ac1 = Account(15)
-ac1.deposit(500)
+def test_account_list():
+    ac1 = Account(15)
+    ac1.deposit(500)
 
-ac2 = Account(476585)
-ac2.deposit(1000)
+    ac2 = Account(476585)
+    ac2.deposit(1000)
 
-# list element refer to original object, i.e., accounts[0] == ac1, accounts[1] == ac2
-accounts = [ac1, ac2]
-for ac in accounts:
-    print(ac)
+    # list element refer to original object, i.e., accounts[0] == ac1, accounts[1] == ac2
+    accounts = [ac1, ac2]
+    for ac in accounts:
+        print(ac)
+
+
+if __name__ == "__main__":
+    test_numpy()
