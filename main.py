2c02dd99032b0394123b5fe082d104140f8f39ac
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-21 21:12:38 -0300

    Adding test_dictionary and test_iterate_dict

--- main.py
+++ main.py
@@ -31,6 +31,50 @@ def test_modifying_set():
     frozen = frozenset(user)
     print(frozen)
 
+def test_dictionary():
+    # creating dictionary from constructor, not usual
+    # dic = dict(jorge = 1, doge = 2, name = 2, come = 1,)
+    dic = {
+        "jorge" : 1,
+        "doge" : 2,
+        "name" : 2,
+        "come" : 1,
+    }
+
+    # adding to dic
+    dic["jefferson"] = 1
+    print(dic)
+    # removing from dic
+    del dic["jefferson"]
+    print(dic)
+
+    print(dic["doge"])
+    # set allows to define a default value if key isn't present
+    print(dic.get("asfd", 0))
+
+def test_iterate_dict():
+    dic = {
+        "jorge" : 1,
+        "doge" : 2,
+        "name" : 2,
+        "come" : 1,
+    }
+    # print dict keys
+    for elem in dic:
+        print(elem)
+    print()
+    # print dict values
+    for elem in dic.values():
+        print(elem)
+    print()
+    # print dict couple
+    for elem in dic.items():
+        print(elem)
+    print()
+    # print dict couple anpacked
+    for key, value in dic.items():
+        print(key, ":", value)
+
 
 if __name__ == "__main__":
-    test_modifying_set()
+    test_iterate_dict()
