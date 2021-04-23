ffb799f8341ef1a3e7835de5849545682ebf5cff
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-22 23:42:40 -0300

    Spliting main.py into test funcions

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from cpf import Cpf
 from cnpj import Cnpj
 
-cpf_doc = 52998224725
-cpf = Cpf(cpf_doc)
-print(cpf)
+def test_cpf():
+    cpf_doc = 52998224725
+    cpf = Cpf(cpf_doc)
+    print(cpf)
 
-cnpj_doc = 35379838000112
-cnpj = Cnpj(cnpj_doc)
-print(cnpj)
+def test_cnpj():
+    cnpj_doc = 35379838000112
+    cnpj = Cnpj(cnpj_doc)
+    print(cnpj)
+
+if __name__ == "__main__":
+    test_doc_factory()
