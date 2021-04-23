39a8163ebe9d769b14a5bc3683bf2f6a82418a55
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-22 23:43:00 -0300

    Adding test_doc_factory

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from cpf import Cpf
 from cnpj import Cnpj
+from doc import Doc
 
 def test_cpf():
     cpf_doc = 52998224725
@@ -10,6 +11,14 @@ def test_cnpj():
     cnpj_doc = 35379838000112
     cnpj = Cnpj(cnpj_doc)
     print(cnpj)
+
+def test_doc_factory():
+    cnpj_doc = 35379838000112
+    cpf_doc = 52998224725
+    doc = Doc.create(cnpj_doc)
+    print(doc)
+
+
 
 if __name__ == "__main__":
     test_doc_factory()
