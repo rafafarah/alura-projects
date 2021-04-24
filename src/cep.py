02aae96949bc0f71f1554fee15e3780d68f60b33
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-24 00:42:14 -0300

    Adding class Address

---
+++ src/cep.py
@@ -0,0 +1,16 @@
+class Address:
+    def __init__(self, cep):
+        cep = str(cep)
+        if self.is_cep_valid(cep):
+            self._cep = cep
+        else:
+            raise ValueError("Invalid CEP")
+
+    def __str__(self):
+        return self.format()
+
+    def is_cep_valid(self, cep):
+        return (8 == len(cep))
+
+    def format(self):
+        return "{}-{}".format(self._cep[:5], self._cep[5:])
