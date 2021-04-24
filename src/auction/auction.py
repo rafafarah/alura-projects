0f625dbb1f0a087430262ce8b4ddb42fa061f85d
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-24 19:13:47 -0300

    Add place_bid in Auction to reduce coupling

--- src/auction/auction.py
+++ src/auction/auction.py
@@ -1,9 +1,15 @@
+from src.auction.bid import Bid
+
 class Auction:
 
     def __init__(self, description):
         self.description = description
         self.__bids = []
 
+    def place_bid(self, bid: Bid):
+        self.__bids.append(bid)
+
     @property
     def bids(self):
-        return self.__bids
+        # return a shallow copy of __bids
+        return self.__bids[:]
