f5f673dab1b19aee0b932371f9c0c64bf3bd0c9a
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-25 03:08:14 -0300

    Refactoring tests to reduce duplicated code

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,30 @@
 from src.auction.user import User
 from src.auction.auction import Auction
+import pytest
 
-def test_should_subtract_from_user_wallet_when_bid_is_placed():
-    jorge = User("Jorge", 100)
-    auction = Auction("Cellphone")
+@pytest.fixture
+def jorge():
+    return User("Jorge", 100)
 
+@pytest.fixture
+def auction():
+    return Auction("Cellphone")
+
+def test_should_subtract_from_user_wallet_when_bid_is_placed(jorge, auction):
     jorge.place_bid(auction, 50)
 
     assert 50 == jorge.wallet
+
+def test_shoud_place_bid_when_wallet_balance_is_greater_than_bid(jorge, auction):
+    jorge.place_bid(auction, 1)
+
+    assert 99 == jorge.wallet
+
+def test_shoud_place_bid_when_wallet_balance_is_equal_to_bid(jorge, auction):
+    jorge.place_bid(auction, 100)
+
+    assert 0 == jorge.wallet
+
+def test_shoud_not_place_bid_when_wallet_balance_is_less_than_bid(jorge, auction):
+    with pytest.raises(ValueError):
+        jorge.place_bid(auction, 150)
