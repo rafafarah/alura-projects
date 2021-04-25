28d937cfd16b8483b305192a8416b27a3eb59324
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-25 01:32:00 -0300

    Adding checks before placing a bid
    - Check if bid is greater than last bid placed
    - Block user from placing two bids in a row

--- src/auction/auction.py
+++ src/auction/auction.py
@@ -10,12 +10,19 @@ class Auction:
         self.lowest_bid = sys.float_info.max
 
     def place_bid(self, bid: Bid):
-        if bid.value > self.highest_bid:
-            self.highest_bid = bid.value
-        if bid.value < self.lowest_bid:
-            self.lowest_bid = bid.value
+        # self.__bids[-1] is equivalent to self.__bids[len(self.__bids) - 1]
+        # place bid if list is either empty or if last user is different from current user
+        if (not self.__bids or
+            self.__bids[len(self.__bids) - 1].user != bid.user and
+            self.__bids[-1].value < bid.value):
+            if bid.value > self.highest_bid:
+                self.highest_bid = bid.value
+            if bid.value < self.lowest_bid:
+                self.lowest_bid = bid.value
 
-        self.__bids.append(bid)
+            self.__bids.append(bid)
+        else:
+            raise ValueError('User cannot place two bids in a row, neither place a smaller than last bid')
 
     @property
     def bids(self):
