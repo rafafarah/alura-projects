73005d58c43842af08e2f9ea48edd71bdbb3c76a
Rafael Farah <rafaelfarahster@gmail.com>
2021-04-25 02:43:25 -0300

    Adding place_bid to User

--- src/auction/user.py
+++ src/auction/user.py
@@ -1,8 +1,20 @@
+from src.auction.bid import Bid
+
 class User:
 
-    def __init__(self, name):
+    def __init__(self, name, wallet=0):
         self.__name = name
+        self.__wallet = wallet
+
+    def place_bid(self, auction, value):
+        bid = Bid(self, value)
+        auction.place_bid(bid)
+        self.__wallet -= value
 
     @property
     def name(self):
         return self.__name
+
+    @property
+    def wallet(self):
+        return self.__wallet
