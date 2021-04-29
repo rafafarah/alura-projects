70fc6aa033ea0a777074b55a00298c80717efd1d
rafafarah <56708538+rafafarah@users.noreply.github.com>
2021-04-28 22:04:51 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// var valorEmDolarTexto = prompt("Qual o valor em dolar que vc quer converter?")
+
+// var valorEmDolar = parseFloat(valorEmDolarTexto)
+
+// // alert("Valor em dolar: " + valorEmDolar)
+
+// var valorEmReal = valorEmDolar * 5.50
+
+// var valorEmRealFixado = valorEmReal.toFixed(2)
+
+// alert("Valor em real: R$" + valorEmRealFixado)
+
+var valor = parseFloat(prompt("Qual o valor em dolar que vc quer converter?")) * 5.50
+
+alert("Valor em real: R$" + valor.toFixed(2))
+
+
+
+
+
+/* Challenges */
+/*Paulo: Refactor; convert lightyears to km
+Gui: Other conversion
+other currencies, temperature
+Rafa: Remove alert e prompt*/
