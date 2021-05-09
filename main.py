193d46a6c4cdf3632f0210b406eff8291f21d6de
Rafael Farah <rafaelfarahster@gmail.com>
2021-05-09 19:26:34 -0300

    Adding test_freq_table,test_scatterplot,test_correlation_g,test_correlation_c,test_most_activated_moa

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 '''
 Aula 1
-Desafio 01: Investigar por que a classe tratamento é tão desbalanceada?
-Desafio 02: Plotar as 5 últimas linhas da tabela
-Desafio 03: Proporção das classes tratamento
-Desafio 04: Quantas tipos de drogas foram investigados
-Desafio 05: Procurar na documentação o método query(pandas)
-Desafio 06: Renomear as colunas tirando o hífen
-Desafio 07: Deixar os gráficos bonitões. (Matplotlib.pyplot)
-Desafio 08: Resumo do que você aprendeu com os dados
+Challenge 01: Investigar por que a classe tratamento é tão desbalanceada?
+Challenge 02: Plotar as 5 últimas linhas da tabela
+Challenge 03: Proporção das classes tratamento
+Challenge 04: Quantas tipos de drogas foram investigados
+Challenge 05: Procurar na documentação o método query(pandas)
+Challenge 06: Renomear as colunas tirando o hífen
+Challenge 07: Deixar os gráficos bonitões. (Matplotlib.pyplot)
+Challenge 08: Resumo do que você aprendeu com os dados
 '''
 data = pd.read_csv('dados/dados_experimentos.zip', compression='zip')
 def test_filter():
-    import matplotlib.pyplot as plt
     # print first rows, default 5
     print(data.head())
 
@@ -47,12 +49,12 @@ def test_filter():
 
 '''
 Aula 2
-Desafio 01: Sort graph
+Challenge 01: Sort graph
     ax = sns.countplot(x = x,
                        data=data_compound,
                        # value_counts(ascending=True)
                        order = data_compound[x].value_counts().index)
-Desafio 02: Change axis name
+Challenge 02: Change axis name
 '''
 def test_compound_graph():
     map = {'droga':'composto'}
@@ -62,8 +64,6 @@ def test_compound_graph():
 
     data_compound = data.query('composto in @compound_ids')
 
-    import seaborn as sns
-    import matplotlib.pyplot as plt
 
     sns.set()
     plt.figure(figsize=(8,6))
@@ -76,42 +76,128 @@ def test_compound_graph():
     plt.show()
 
 '''
-Desafio 03: Plot graphs side by side
+Challenge 03: Plot graphs side by side
     fig, axes = plt.subplots(1, 2)
     data['g-0'].hist(bins=200, ax=axes[0])
     data['g-19'].hist(bins=200, ax=axes[1])
-Desafio 04: Plot graphs with seaborn
-Desafio 05: Check stats in describe()
+Challenge 04: Plot graphs with seaborn
+Challenge 05: Check stats in describe()
 '''
 def test_histogram():
-    import matplotlib.pyplot as plt
     data['g-0'].hist(bins=200)
     data['g-19'].hist(bins=200)
     plt.show()
 
 # g: gene expression
 def test_describe_g():
-    import matplotlib.pyplot as plt
     data.loc[:, 'g-0':'g-771'].describe().T.hist(bins=30)
     plt.show()
 
 # c: cell line
 def test_describe_c():
-    import matplotlib.pyplot as plt
     data.loc[:, 'c-0':'c-99'].describe().T.hist(bins=30)
     plt.show()
 
 '''
-Desafio 06: Reflect about graph size and visualization
-Desafio 07: Plot other boxplots and histogram
+Challenge 06: Reflect about graph size and visualization
+Challenge 07: Plot other boxplots and histogram
 '''
 def test_boxplot():
-    import seaborn as sns
-    import matplotlib.pyplot as plt
     sns.boxplot(x='g-0', data=data)
     sns.boxplot(y='g-0', x='tratamento', data=data)
     plt.show()
 
+'''
+Aula 3
+Challenge 01: Create frequency table with pandas.groupby()
+Challenge 02: Normalize frequency table by columns
+Challenge 03: Test other aggfunc
+Challenge 04: Explote melt tool
+'''
+def test_freq_table():
+    # freq table 'dose' x 'tempo'
+    print(pd.crosstab(data['dose'], data['tempo']))
+    # freq table ('dose' x 'tempo') x 'tratamento'
+    print(pd.crosstab([data['dose'], data['tempo']], data['tratamento']))
+    # freq table ('dose' x 'tempo') x 'tratamento', normalized by 'index'
+    print(pd.crosstab([data['dose'], data['tempo']], data['tratamento'], normalized='index'))
+    # freq table ('dose' x 'tempo') x 'tratamento', for mean of 'g-0'
+    print(pd.crosstab([data['dose'], data['tempo']], data['tratamento'], values=data['g-0'], aggfunc='mean'))
+
+def test_scatterplot():
+    # plot graph maping 'g-0' and 'g-3' as coordinates
+    sns.scatterplot(x='g-0', y='g-3', data=data)
+    # plot graph maping 'g-0' and 'g-8' as coordinates
+    sns.scatterplot(x='g-0', y='g-8', data=data)
+    # plot graph maping 'g-0' and 'g-8' as coordinates and draw tendency line in red
+    sns.lmplot(data=data, x='g-0', y='g-8', line_kws={'color':'red'})
+    # plot graph maping 'g-0' and 'g-8' as coordinates, separating for 'tratamento'
+    sns.lmplot(data=data, x='g-0', y='g-8', line_kws={'color':'red'}, col='tratamento')
+    # plot graph maping 'g-0' and 'g-8' as coordinates, separating for 'tratamento' and 'tempo'
+    sns.lmplot(data=data, x='g-0', y='g-8', line_kws={'color':'red'}, col='tratamento', row='tempo')
+    plt.show()
+
+def test_correlation_g():
+    # # print correlation of all gene expressions
+    # print(data.loc[:, 'g-0':'g-771'].corr())
+
+    corr = data.loc[:, 'g-0':'g-50'].corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(11, 9))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, mask=mask, cmap=cmap, center=0,
+        square=True, linewidths=.5, cbar_kws={'shrink': .5})
+
+    plt.show()
+
+'''
+Challenge 05: Check correlation between 'g-X' and 'c-X'
+Challenge 06: Study how heatmap is built
+'''
+def test_correlation_c():
+    # # print correlation of all gene expressions
+    # print(data.loc[:, 'c-0':'c-771'].corr())
+
+    corr = data.loc[:, 'c-0':'c-50'].corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(11, 9))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, mask=mask, cmap=cmap, center=0,
+        square=True, linewidths=.5, cbar_kws={'shrink': .5})
+
+    plt.show()
+
+'''
+Aula 4
+Challenge 01: Find top 10 actions of moa (sulfix inhibitor, antagonist, agonist, ...)
+'''
+def test_most_activated_moa():
+    results = pd.read_csv('dados/dados_resultados.csv')
+    print(results['acetylcholine_receptor_agonist'].unique())
+
+    # moa =  mechanism of activation
+    # sum moa columns and sort descending order
+    # select int64 types to ignore first column (alphanumeric 'id')
+    # alternative way dropping 'id': results.drop('id', axis=1).sum().sort_values(ascending=False)
+    count_moa = results.select_dtypes('int64').sum().sort_values(ascending=False)
+    print(count_moa)
+
 
 if __name__ == "__main__":
-    test_boxplot()
+    test_most_activated_moa()
